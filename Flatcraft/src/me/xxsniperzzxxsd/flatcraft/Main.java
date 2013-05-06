0802ab94af324f93f3a34e217482ae111198b047
Owen <computewizkid4229@outlook.com>
2013-05-06 17:38:38 +0100

    Main Added
    Added all binary and code

---
+++ Flatcraft/src/me/xxsniperzzxxsd/flatcraft/Main.java
@@ -0,0 +1,32 @@
+package me.xxsniperzzxxsd.flatcraft;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	
+	
+	
+	public void onEnable(){
+		System.out.println("Flatcraft Plugin Loaded!");
+	
+	}
+	public void onDisable(){
+		System.out.println("Flatcraft Plugin Disabled");
+	
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		Player player = (Player)sender;
+		if(cmd.getName().equalsIgnoreCase("Flatcraft")){
+			player.sendMessage(ChatColor.BOLD+"Flatcraft is a server Owned by the awesome Cwk!");
+		}
+		return true;
+	}
+
+
+}
---
+++ Flatcraft/src/me/xxsniperzzxxsd/flatcraft/PlayerListener.java
@@ -0,0 +1,24 @@
+package me.xxsniperzzxxsd.flatcraft;
+
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener {
+
+	public Main plugin;
+	
+	public PlayerListener(Main instance){
+		plugin = instance;
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerJoin(PlayerJoinEvent e){
+		e.getPlayer().sendMessage(ChatColor.UNDERLINE+"Welcome to Flatcraft");
+		
+	}
+
+}
+
